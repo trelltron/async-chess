d9400f303dd646ca2d4a24eb0d59da26248af7d6
Tom Ellis <tom.ellis@bath.edu>
2019-04-24 20:20:22 +0100

    Basic styling for login flow, added logout button and navbar

--- client/src/index.js
+++ client/src/index.js
@@ -86,24 +86,38 @@ class AuthModal extends Component {
   render() {
     return (
       <div className= 'auth-modal'>
-        { this.state.error ? this.state.error : 'Please Sign in. '}
-        { this.state.token ?
-          <SignupForm 
-            token={this.state.token}
-            tokenInvalid={this.tokenInvalid}
-            loginSuccess={this.props.loginSuccess}
-          /> :
-          <GoogleLogin
-            clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-            buttonText="Login"
-            responseType="id_token"
-            onSuccess={(data) => this.attemptLogin(data)}
-            onFailure={(data) => console.log(data)}
-            cookiePolicy={'single_host_origin'}
-          />
-        }
+        <div className= 'auth-modal-inner'>
+          <div className='auth-modal-text'>
+            { this.state.error ? this.state.error : 'Please Sign in. ' }
+          </div>
+          <div className='auth-modal-content'>
+            { this.state.token ?
+              <SignupForm 
+                token={this.state.token}
+                tokenInvalid={this.tokenInvalid}
+                loginSuccess={this.props.loginSuccess}
+              /> :
+              <GoogleLogin
+                clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+                buttonText="Login"
+                responseType="id_token"
+                onSuccess={(data) => this.attemptLogin(data)}
+                onFailure={(data) => console.log(data)}
+                cookiePolicy={'single_host_origin'}
+              />
+            }
+          </div>
+        </div>
       </div>
     );
+  }
+}
+
+class Navbar extends Component {
+  render() {
+    return (<div className='navbar'> 
+      <button className='logout-button' onClick={this.props.logout}> Logout </button> 
+    </div>)
   }
 }
 
@@ -117,12 +131,6 @@ class Main extends Component {
   }
   componentDidMount() {
     this.fetchUserData();
-  }
-  render() {
-    return <div className='app-root'>
-      <span>Main App Placeholder</span>
-      { this.state.auth_required ? <AuthModal loginSuccess={(user) => this.loginSuccess(user)}/> : null }
-    </div>
   }
   fetchUserData() {
     fetch('/api/me').then((response) => {
@@ -139,6 +147,19 @@ class Main extends Component {
   loginSuccess(user) {
     this.setState({ auth_required: false });
     this.fetchUserData();
+  }
+  logout() {
+    fetch('/api/logout', {method: 'POST'}).then((response) => {
+      console.log(response);
+      window.location.reload()
+    });
+  }
+  render() {
+    return <div className='app-root'>
+      <Navbar logout={() => this.logout() }/>
+      <div className='app-wrapper'><span className='app-placeholder'>Main App Placeholder</span></div>
+      { this.state.auth_required ? <AuthModal loginSuccess={(user) => this.loginSuccess(user)}/> : null }
+    </div>
   }
 }
 
