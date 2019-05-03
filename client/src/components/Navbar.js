4e1b9c1441aad219cb116c7a105eff278e9f4233
Tom Ellis <tom.ellis@bath.edu>
2019-05-03 17:01:06 +0100

    Display current user's nickname within app

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,16 +1,33 @@
 import React from 'react';
+import { connect } from 'react-redux'
 
 import './Navbar.css';
 
-const logout = () =>  {
-  fetch('/api/v1/auth/logout', {method: 'POST'}).then((response) => {
-    console.log(response);
-    window.location.reload()
-  });
+class Navbar extends React.Component {
+  logout() {
+    fetch('/api/v1/auth/logout', {method: 'POST'}).then((response) => {
+      window.location.reload()
+    });
+  }
+  render() {
+    return (
+      <div className='navbar'> 
+        { this.props.user ? 
+            <span className="player-nickname"> nickname: { this.props.user.nickname } </span>
+          :
+            null
+        }
+        <button className='ac-button' onClick={this.logout}> Logout </button> 
+      </div>
+    )
+  }
 }
 
-export default () => (
-  <div className='navbar'> 
-    <button className='ac-button' onClick={logout}> Logout </button> 
-  </div>
-)
+
+const mapStateToProps = state => ({
+  user: state.auth.user
+})
+
+export default connect(
+  mapStateToProps
+)(Navbar)
